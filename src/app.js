2583d97100596bddbbacda8853f379e91fc9d727
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-17 23:32:21 -0300

    feat: update repository

--- src/app.js
+++ src/app.js
@@ -31,7 +31,28 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: "Repository not found",
+    });
+  }
+
+  const repositoryUpdatedFields = { title, url, techs };
+
+  const updatedRepository = {
+    ...repositories[repositoryIndex],
+    ...repositoryUpdatedFields
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
